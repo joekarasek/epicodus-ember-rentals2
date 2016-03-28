20663580150b4f142603d73b16c481f91e68dd5e
brooke hurford and dustin davis <hiitsdustindavis@gmail.com>
2016-03-28 11:13:11 -0700

    Add save and delete functionality

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -8,6 +8,12 @@ export default Ember.Component.extend({
     },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
     }
   }
 });
+//The "imageShow" and "imageHide" functions correspond to action helpers of the same names in rental-tile.hbs.
--- app/routes/index.js
+++ app/routes/index.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model() { // Model hook. This returns our array, and makes it available to the templates and components that correspond to that route. In this case, that's the index.hbs.
     return this.store.findAll('rental');
   },
+  actions: {
+    save3(params) {
+      var newRental = this.store.createRecord('rental', params);
+      newRental.save();
+      this.transitionTo('index');
+    },
+    destroyRental(rental) {
+      rental.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
